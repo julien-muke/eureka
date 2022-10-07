9461e337e1135b00dfb75d729a9987cd6693ed81
julien-muke <julien.muke@gmail.com>
2022-10-07 08:31:12 +0200

    setting sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import {
     createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-uo-form.component";
+
 const SignIn = () => {
 
 
@@ -16,6 +18,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+            <SignUpForm />
         </div>
     );
 };
