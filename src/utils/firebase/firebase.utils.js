9fca2fa357471fc1fec634d44c0f09541da77204
julien-muke <julien.muke@gmail.com>
2022-10-07 06:10:24 +0200

    added our sign in component as well as firebase utils and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -39,8 +39,32 @@ const firebaseConfig = {
  export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef);
-
     const userSnapShot = await getDoc(userDocRef);
-    console.log(userSnapShot.exists());
+
+    //if user data does not exists
+
+    // create /set the document with the data from userAuth in my collection
+
+   if(!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+   }
+
+     // if user data exists
+
+    // return userDocRef
+
+    return userDocRef;
+
+
   }
