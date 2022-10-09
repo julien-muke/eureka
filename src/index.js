e3998653bc765b24d80775dd9ddad160e10740a7
julien-muke <julien.muke@gmail.com>
2022-10-10 00:39:22 +0200

    user context

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { UserProvider } from './contexts/user.context';
+
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,9 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
       <App />
-    </BrowserRouter>
-    
+      </UserProvider>
+    </BrowserRouter> 
   </React.StrictMode>
 );
 
