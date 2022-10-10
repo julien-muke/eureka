2f3c777743458dc14d28f1283b22d4db39a516f6
julien-muke <julien.muke@gmail.com>
2022-10-11 00:02:17 +0200

    Observer: onAuthStateChange

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -29,8 +29,8 @@ const SignInForm = () => {
     };
 
     const signInWithGoogle = async () => {
-        const { user } = await signInWithGooglePopup(); 
-        await createUserDocumentFromAuth(user);
+        const { user } = await signInWithGooglePopup();
+        createUserDocumentFromAuth(user);
      };
 
     const handleSubmit = async (event) => {
@@ -41,9 +41,7 @@ const SignInForm = () => {
                 email, 
                 password
                 );
-               setCurrentUser(user);
-
-           resetFormFields();
+                resetFormFields();
         } catch(error) {
             switch(error.code) {
                 case 'auth/wrong-password':
