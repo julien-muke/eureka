f2ffb5b07849b8b2dc25b00a6cba2a4214fb3128
julien-muke <julien.muke@gmail.com>
2022-10-12 06:37:02 +0200

    Cart Icon & Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,9 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
 
 import { ReactComponent as CrawnLogo } from '../../assets/crown.svg';
@@ -28,8 +32,10 @@ const Navigation = () => {
                   <Link className='nav-link' to='/auth'>
                   SIGN IN
               </Link>
-                )};
+                )}
+                <CartIcon />
          </div>
+         <CartDropdown />
          </div>
         <Outlet />
       </Fragment>
