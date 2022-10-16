c228b9b93da098ec0c8052b2dec77eb3606f3754
julien-muke <julien.muke@gmail.com>
2022-10-17 01:32:59 +0200

    Styled-Components - Button

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -8,19 +8,28 @@ google sign in button
 
 */
 
-import './button.styles.scss';
+import { 
+    BaseButton, 
+    GoogleSignInButton, 
+    InvertedButton, 
+} from './button.styles';
 
-const BUTTON_TYPE_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
+    base: 'base',
     google: 'google-sign-in',
-    inverted: 'inverted'
-}
-
-const button = ({ children, buttonType, ...otherProps }) => {
-    return <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-    {...otherProps}
-    >
-    {children}
-    </button>
+    inverted: 'inverted',
 };
 
-export default button;
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => 
+    ({
+      [BUTTON_TYPE_CLASSES.base]: BaseButton,
+      [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton, 
+      [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,   
+    }[buttonType]);
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+    const CustomButton = getButton(buttonType);
+    return <CustomButton {...otherProps}>{children}</CustomButton>;
+};
+
+export default Button;
