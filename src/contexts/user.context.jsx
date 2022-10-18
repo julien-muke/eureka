e51499bd3dc799dd8bff017b68cf6500328990e8
julien-muke <julien.muke@gmail.com>
2022-10-18 22:19:30 +0200

    adding User Reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+// import { act } from "react-dom/test-utils";
 
 import { 
     onAuthStateChangedListener, 
@@ -7,12 +8,40 @@ import {
 
 // as the actual value you want to access
 export const UserContext = createContext({
-    currentUser: null,
     setCurrentUser: () => null,
+    currentUser: null,
+    
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER' 
+}
+
+const userReducer = (state, action) => {
+   const { type, payload } = action; 
+   
+   switch(type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+        return {
+            ...state,
+            currentUser: payload,
+        };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+   }
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE );
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
