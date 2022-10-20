cb69afac6eba2fe44c623f8d067f72406152dafa
julien-muke <julien.muke@gmail.com>
2022-10-20 22:35:43 +0200

    installing redux and react redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -16,25 +16,23 @@ export const UserContext = createContext({
 
 export const USER_ACTION_TYPES = {
     SET_CURRENT_USER: 'SET_CURRENT_USER' 
-}
+};
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
 
 const userReducer = (state, action) => {
    const { type, payload } = action; 
    
    switch(type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
-        return {
-            ...state,
-            currentUser: payload,
-        };
+        return {...state, currentUser: payload };
         default:
             throw new Error(`Unhandled type ${type} in userReducer`);
    }
-}
-
-const INITIAL_STATE = {
-    currentUser: null,
 };
+
 
 export const UserProvider = ({ children }) => {
     const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE );
---
+++ src/store/root-reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import { useReducer, userReducer } from './user/user.reducer';
+
+export const rootReducer = combineReducers({
+    user: userReducer
+})
