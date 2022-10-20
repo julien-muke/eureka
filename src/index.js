e0980a6557067a597a4ee5a5747cdafae5d2e206
julien-muke <julien.muke@gmail.com>
2022-10-20 23:22:16 +0200

    React-Redux: Creating User Reducer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { UserProvider } from './contexts/user.context';
-
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -18,13 +16,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
      <BrowserRouter>
-      <UserProvider>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
      </BrowserRouter>
     </Provider> 
   </React.StrictMode>
