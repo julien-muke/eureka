9214ad91ab6669a565bf581fe656764eaaac19e6
julien-muke <julien.muke@gmail.com>
2022-10-21 07:11:18 +0200

    Categories Reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
-import { useReducer, userReducer } from './user/user.reducer';
+import { userReducer } from './user/user.reducer';
+import { categoriesReducer } from "./categories/category.reducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    categories: categoriesReducer,
 });
