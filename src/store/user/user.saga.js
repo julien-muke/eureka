b9e04f0a59b765b4f2b4a076a2203f626e263c1c
julien-muke <julien.muke@gmail.com>
2022-10-26 08:48:10 +0200

    fixing errors

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -4,7 +4,7 @@ import { USER_ACTION_TYPES } from './user.types';
 
 import { signInSuccess, signInFailed } from './user.action';
 
-import { getCurrentUser, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { getCurrentUser, createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
     try {
@@ -19,6 +19,15 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
     }
 }
 
+export function* signInWithGoogle() {
+    try {
+        const { user } = yield call(signInWithGoogle);
+        yield call(getSnapshotFromUserAuth, user);
+    } catch(error) {
+        yield put(signInFailed(error));
+    }
+}
+
 export function* isUserAuthenticated() {
     try {
         const userAuth = yield call(getCurrentUser);
@@ -29,10 +38,14 @@ export function* isUserAuthenticated() {
     }
 }
 
+export function* onGoogleSignInStart() {
+    yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
 export function* onCheckUserSession() {
     yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
 export function* userSagas() {
-    yield all([call(onCheckUserSession)]);
+    yield all([call(onCheckUserSession), call(onGoogleSignInStart)]);
 }
