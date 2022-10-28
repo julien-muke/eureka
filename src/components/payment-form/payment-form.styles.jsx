8989f2e09115a20ba960804dfe63a72c8bf19dc6
julien-muke <julien.muke@gmail.com>
2022-10-28 08:05:55 +0200

    Setting up Our PaymentForm

---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PaymentFormContainer = styled.div`
+height: 300;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+export const FormContainer = styled.form`
+height: 100;
+min-width: 500;
+`;
