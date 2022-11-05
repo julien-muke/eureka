a8bc136af6c45181ff8ce6e79900216cc1c42e1c
julien-muke <julien.muke@gmail.com>
2022-11-05 23:46:44 +0200

    added progressive web apps

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ import './index.scss';
 import App from './App';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
 root.render(
   <React.StrictMode>
@@ -23,10 +25,13 @@ root.render(
      </BrowserRouter>
      </PersistGate>
     </Provider> 
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+serviceWorkerRegistration.register();
